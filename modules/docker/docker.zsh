cf2923e6f33bba2dab5f473ce024c872341e8f7c
Sébastien Balayn <sebastien@loungeup.com>
2021-07-15 15:08:09 +0200

    add docker aliases & szs instead of complicated szshrc

---
+++ modules/docker/docker.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# can be upgraded with https://gist.github.com/jgrodziski/9ed4a17709baad10dbcd4530b60dfcbb
+
+alias dc='docker compose'
+alias dcu='docker compose up'
+alias dcud='docker compose up -d'
+alias dcd='docker compose down'
+alias dex='docker exec -it'
+alias dins='docker inspect'
+alias dim='docker images'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
--- modules/docker/install.zsh
+++ modules/docker/install.zsh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
 
 brew cask install docker
-brew cask install captain
 npm install -g dockly
